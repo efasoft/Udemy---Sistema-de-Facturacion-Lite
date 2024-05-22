0e530ed36e854efa987961aa392d2387757ca4f9
efasoft <efasoft@hotmail.com>
2024-05-22 23:05:48 +0200

    Hasta el Menu - Activo y Desactivo opciones

--- bases/urls.py
+++ bases/urls.py
@@ -1,9 +1,27 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from bases.views import *
 
 app_name = "config"
 
 urlpatterns = [
-    path('',Home.as_view(),name='home'),    
+    path('',Home.as_view(),name='home'),
+    path('login/',auth_views.LoginView.as_view(template_name='bases/login.html'),
+        name='login'),
+    path('logout/',
+         auth_views.LogoutView.as_view(template_name='bases/login.html'),
+         name='logout'),
+    
+
+
+    path('catalogos/categorias',
+	 Home.as_view(),
+	 name='categorias'),
+    path('catalogos/subcategorias',
+        Home.as_view(),
+        name='subcategorias'),
+    path('movimientos/compras',
+        Home.as_view(),
+        name='compras'),       
 ]
